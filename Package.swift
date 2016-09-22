670d4580e9b13db6d56d750902fe99d4708181ea
Pablo Lopez <plopez@xebia.fr>
2016-09-22 16:03:49 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -5,8 +5,5 @@ let package = Package(
     dependencies: [
         // Pure-Swift Redis client implemented from the original protocol spec.
         .Package(url: "https://github.com/vapor/redbird.git", majorVersion: 0, minor: 11),
-
-        // Importing Vapor for access to the Provider protocol.
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 1, minor: 0)
     ]
 )
